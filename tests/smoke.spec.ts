0f8555bc02488aa5618ce6f33b2116e4a1374eb2
jill64 <intents.turrets0h@icloud.com>
2023-12-10 12:42:34 +0900

    extend test

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,5 +1,13 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test('smoke', async ({ page }) => {
   await page.goto('/')
+
+  await expect(page.getByTestId('hydrated')).toBeAttached()
+
+  await expect(
+    page.getByRole('heading', { name: '@jill64/npm-demo-layout' })
+  ).toBeVisible()
+
+  await expect(page.getByText('This is Page')).toBeVisible()
 })
